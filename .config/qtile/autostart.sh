33fefbe4c221d337146d8c83f6c75e5b06d853a1
Ethan Dang <3thandang@gmail.com>
2024-09-13 12:02:12 -0400

    got rid of unnecessary files

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,6 +3,9 @@
 pactl set-sink-mute @DEFAULT_SINK@ 0
 pactl set-sink-volume @DEFAULT_SINK@ 60%
 
+xinput set-prop "device" "libinput Click Method Enabled" 0 1
+
+udiskie --tray &
 dunst &
 picom &
 mpd &
